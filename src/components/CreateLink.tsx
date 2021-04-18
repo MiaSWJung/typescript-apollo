844a0ef1772bb6778d153ff39039738ff6e5bf1b
MiaSWJung <81947118+MiaSWJung@users.noreply.github.com>
2021-04-18 23:18:40 +0900

    Update CreateLink.tsx

--- src/components/CreateLink.tsx
+++ src/components/CreateLink.tsx
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import {useMutation, gql} from "@apollo/client";
 import {useHistory} from "react-router";
+import { LINKS_PER_PAGE } from "../constants";
+import FEED_QUERY from './LinkList'
 
 const CREATE_LINK_MUTATION = gql`
   mutation PostMutation(
@@ -27,7 +29,23 @@ const CreateLink : React.FC = ()=> {
     variables : {
       description : formState.description,
       url : formState.url
-    }
+    },
+    update: (cache, { data: {post}})=>{
+      const take = LINKS_PER_PAGE;
+      const skip = 0;
+      const orderBy = { createAt : 'desc' }
+      const data = cache.readQuery({
+        query: FEED_QUERY,
+        variables : { take, skip, orderBy }
+      });
+
+      cache.writeQuery({
+        query : FEED_QUERY,
+        data : { feed: { links : [post, ...data.feed.links] } },
+        variables : { take, skip, orderBy }
+      });
+    },
+    onCompleted : () => history.push('/new/1')
   })
 
   return (
