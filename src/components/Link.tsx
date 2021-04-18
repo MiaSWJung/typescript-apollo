3c5a4f1d4863ef452cfd9ee74446b6e98690adf7
MiaSWJung <81947118+MiaSWJung@users.noreply.github.com>
2021-04-18 23:18:07 +0900

    Update Link.tsx

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,20 +1,125 @@
+import { useMutation, gql } from "@apollo/client";
 import React from "react";
+import { AUTH_TOKEN, LINKS_PER_PAGE } from '../constants'
+import {timeDifferenceForDate} from '../utils'
+import FEED_QUERY from './LinkList'
+
+interface Author{
+  id : number;
+  name : string;
+}
+
+interface User{
+  id : number;
+}
+
+interface Vote{
+  id : number;
+  user : User;
+}
 
 interface Link{
-    description : string;
-    url : string;
+  id : number;
+  description : string;
+  url : string;
+  votes : Array<Vote>;
+  createdAt : Date;
+  postedBy : Author;
 }
 
 interface LinkProps{
-    link : Link
+  link : Link
+  index : number;
 }
+
+interface FeedLink{
+  id : number
+  votes : Array<Vote>;
+}
+
+interface Feed{
+  links : Array<FeedLink>
+}
+
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      link {
+        id
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
 
 const Link : React.FC<LinkProps>  = (props) =>{
   const {link} = props;
+  const authToken = localStorage.getItem(AUTH_TOKEN);
+  const take = LINKS_PER_PAGE;
+  const skip = 0;
+  const orderBy = {createdAt : 'desc'};
+
+  const [vote] = useMutation(VOTE_MUTATION, {
+    variables: { linkId : link.id},
+    update(cache, { data: { vote } }){
+      const { feed } = cache.readQuery({ query : FEED_QUERY })
+      const updatedLinks = feed.links.map((feedLink : FeedLink)=> {
+        if (feedLink.id === link.id){
+          return {
+            ...feedLink,
+            votes : [...feedLink.votes, vote]
+          }
+        }
+        return feedLink
+      })
+      
+      cache.writeQuery({
+        query: FEED_QUERY,
+        data : {
+          feed : {
+            links : updatedLinks
+          }
+        }
+      })
+    }
+  })
+
+
+
   return (
-    <div>
-      <div>
-        {link.description} ({link.url})
+    <div className="flex mt2 items-start">
+      <div className="flex items-center">
+        <span className="gray">{props.index +1 } . </span>
+        {authToken && (
+          <div 
+            className="ml1 gray f11"
+            style={{cursor: 'pointer'}}
+            onClick={()=>{vote()}}
+          >
+            ▲
+          </div>
+        )}
+      </div>
+      <div className='ml1'>
+        <div>
+          {link.description} ({link.url})
+        </div>
+        {authToken && (
+          <div className="f6 lh-copy gray">
+            {link.votes.length} vote | by{' '}
+            {link.postedBy ? link.postedBy.name : 'Unknown'}{' '}
+            {timeDifferenceForDate(link.createdAt)}
+          </div>
+        )}
       </div>
     </div>
   )
