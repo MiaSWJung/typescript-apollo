e45e6c28ca9e238378747961e3787ad57a2b4d6c
MiaSWJung <mia.jung@linethree.com>
2021-04-19 18:24:52 +0900

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,52 @@
 import React from "react";
-import {useHistory} from "react-router";
-import {Link, withRouter} from "react-router-dom";
-import {AUTH_TOKEN} from "../constants";
+import { useHistory } from "react-router";
+import { Link, withRouter } from "react-router-dom";
+import { AUTH_TOKEN } from "../constants";
 
-const Header = () =>{
+const Header = () => {
   const history = useHistory();
   const authToken = localStorage.getItem(AUTH_TOKEN);
   return (
     <div className="flex pa1 justify-between nowrap orange">
       <div className="flex flex-fixed black">
-        <div className="fw7 mr1"> Hacker News </div>
-        <Link to='/' className='ml1 no-underline black'> new </Link>
+        <div className="fw7 mr1"> Hacker News</div>
+        <Link to="/" className="ml1 no-underline black">
+          {" "}
+          new{" "}
+        </Link>
         <div className="ml1">|</div>
-        <Link to='/top' className='ml1 no-underline black'> top </Link>
+        <Link to="/top" className="ml1 no-underline black">
+          {" "}
+          top{" "}
+        </Link>
         <div className="ml1">|</div>
-        <Link to='/search' className='ml1 no-underline black'> search </Link>
-        <div className="ml1">|</div>
-        {authToken  &&  (
+        <Link to="/search" className="ml1 no-underline black">
+          {" "}
+          search{" "}
+        </Link>
+        {authToken && (
           <div className="flex">
             <div className="ml1">|</div>
-            <Link to='/create' className="ml1 no-underline black" > submit </Link>
+            <Link to="/create" className="ml1 no-underline black">
+              {" "}
+              submit{" "}
+            </Link>
           </div>
         )}
       </div>
       <div className="flex flex-fixed">
         {authToken ? (
-          <div className="ml1 pointer black" onClick={()=>{
-            localStorage.removeItem(AUTH_TOKEN);
-            history.push('/');
-          }}>
+          <div
+            className="ml1 pointer black"
+            onClick={() => {
+              localStorage.removeItem(AUTH_TOKEN);
+              history.push("/");
+            }}
+          >
             logout
           </div>
         ) : (
-          <Link
-            to='/login'
-            className='ml1 no-underline black'
-          >
+          <Link to="/login" className="ml1 no-underline black">
             login
           </Link>
         )}
