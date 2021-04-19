87dd35be7e11d69d1e345982dccb99833bc423a1
MiaSWJung <mia.jung@linethree.com>
2021-04-19 18:24:27 +0900

    Update queries.js

--- src/queries.js
+++ src/queries.js
@@ -1,19 +1,41 @@
-import {gql} from "@apollo/client";
+import { gql } from "@apollo/client";
+
+// Search
+export const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      id
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`;
 
 // CreateLink
 export const CREATE_LINK_MUTATION = gql`
-  mutation PostMutation(
-    $description: String!
-    $url: String!
-  ) {
-    post(description: $description, url: $url){
+  mutation PostMutation($description: String!, $url: String!) {
+    post(description: $description, url: $url) {
       id
       createdAt
       url
       description
     }
   }
-`
+`;
 
 // Link
 export const VOTE_MUTATION = gql`
@@ -38,52 +60,62 @@ export const VOTE_MUTATION = gql`
 
 // LinkList
 export const FEED_QUERY = gql`
-  query {
-    feed {
+  query FeedQuery($take: Int, $skip: Int, $orderBy: LinkOrderByInput) {
+    feed(take: $take, skip: $skip, orderBy: $orderBy) {
       id
       links {
         id
         createdAt
-        description
         url
+        description
         postedBy {
           id
           name
         }
         votes {
           id
-          user{
+          user {
             id
           }
         }
       }
+      count
     }
   }
-`
+`;
+
+export const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`;
 
 // Login
 export const SIGNUP_MUTATION = gql`
-  mutation SignupMutation(
-    $email: String!
-    $password: String!
-    $name: String!
-  ) {
-    signup(
-      email: $email
-      password: $password
-      name: $name
-    ) {
+  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+    signup(email: $email, password: $password, name: $name) {
       token
     }
   }
 `;
 
-
 export const LOGIN_MUTATION = gql`
-  mutation LoginMutation(
-    $email: String!
-    $password: String!
-  ) {
+  mutation LoginMutation($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
     }
