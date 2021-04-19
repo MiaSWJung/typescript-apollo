e6dd5d5299db1bbf7ee8cca9ecdcdc5c74b9ba98
MiaSWJung <mia.jung@linethree.com>
2021-04-19 14:02:44 +0900

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,41 @@
+
+export interface Author{
+  id : string;
+  name : string;
+}
+
+export interface User{
+  id : string;
+}
+
+export interface Vote{
+  id : string;
+  user : User;
+}
+
+export interface Link{
+  id : string;
+  description : string;
+  url : string;
+  votes : Array<Vote>;
+  createdAt : Date;
+  postedBy : Author;
+}
+
+export interface LinkProps{
+  link : Link
+  index : number;
+}
+
+export interface FeedLink{
+  id : string
+  votes : Array<Vote>;
+}
+
+export interface Feed{
+  feed : {
+    id : string
+    links : Array<FeedLink>
+    count : number
+  }
+}
