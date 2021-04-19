1aeb0c7920809d6a2950b4139148051b48a682b1
MiaSWJung <mia.jung@linethree.com>
2021-04-19 18:24:18 +0900

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,41 +1,47 @@
+import { OperationTypeNode } from "graphql";
 
-export interface Author{
-  id : string;
-  name : string;
+export interface Author {
+  id: string;
+  name: string;
 }
 
-export interface User{
-  id : string;
+export interface User {
+  id: string;
 }
 
-export interface Vote{
-  id : string;
-  user : User;
+export interface Vote {
+  id: string;
+  user: User;
 }
 
-export interface Link{
-  id : string;
-  description : string;
-  url : string;
-  votes : Array<Vote>;
-  createdAt : Date;
-  postedBy : Author;
+export interface Link {
+  id: string;
+  description: string;
+  url: string;
+  votes: Array<Vote>;
+  createdAt: Date;
+  postedBy: Author;
 }
 
-export interface LinkProps{
-  link : Link
-  index : number;
+export interface LinkProps {
+  link: Link;
+  index: number;
 }
 
-export interface FeedLink{
-  id : string
-  votes : Array<Vote>;
+export interface FeedLink {
+  id: string;
+  votes: Array<Vote>;
 }
 
-export interface Feed{
-  feed : {
-    id : string
-    links : Array<FeedLink>
-    count : number
-  }
+export interface Feed {
+  feed: {
+    id: string;
+    links: Array<Link>;
+    count: number;
+  };
 }
+
+export type Definition = {
+  kind: string;
+  operation?: OperationTypeNode;
+};
