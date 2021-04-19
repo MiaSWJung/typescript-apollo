f8fe2b8145c5987c9bf60a866643a36085c472b5
MiaSWJung <mia.jung@linethree.com>
2021-04-19 18:25:07 +0900

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,31 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import CreateLink from "./CreateLink";
-import {Switch, Route} from "react-router-dom";
-import '../styles/App.css';
+import { Switch, Route, Redirect } from "react-router-dom";
+import "../styles/App.css";
 import Header from "./Header";
 import LinkList from "./LinkList";
-import Login from './Login';
+import Login from "./Login";
+import Search from "./Search";
 
-// class App extends Component {
-//   render(){
-//     // return <LinkList />;
-//     return <CreateLink />;
-//   }
-// }
-
-const App = () =>{
+const App = () => {
   return (
     <div className="center w85">
       <Header />
       <div className="ph3 pv1 background-grey">
         <Switch>
-          <Route exact path='/' component={LinkList} />
-          <Route exact path='/create' component={CreateLink} />
-          <Route exact path='/login' component={Login} />
+          <Route exact path="/" component={LinkList} />
+          {/*<Route exact path='/' render={() => <Redirect to="/new/1" />} />*/}
+          <Route exact path="/create" component={CreateLink} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/new/:page" component={LinkList} />
         </Switch>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
